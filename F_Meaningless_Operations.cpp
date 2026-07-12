b4a57b7db31db9f75c78d5b045921c947b04f51d
rishabh sinha <rishabh.sinha0709@gmail.com>
2026-07-12 15:54:26 +0530

    number theory practice upsolve

--- F_Meaningless_Operations.cpp
+++ F_Meaningless_Operations.cpp
@@ -11,23 +11,62 @@ int main() {
     int t;cin>>t;
     while(t--){
 
-        ll n; cin>>n;
+        ll a; cin>>a;
 
-        int x = __builtin_popcountll(n); // no. of set bits
+        // int x = __builtin_popcountll(n); // no. of set bits
 
-        bool even;
+        // bool even;
 
-        if(x%2){
-            even = false;
+        // if(x%2){
+        //     even = false;
+        // }
+        // else even = true;
+
+        // if(!even){
+        //     cout << gcd(a,1)<<"\n";
+        // }
+        // else{
+        //     
+        // }
+
+        int k=0;
+
+        while((1<<k)<=a){
+            k++;
         }
-        else even = true;
 
-        if(!even){
-            cout << gcd(a,1)<<"\n";
+        int mx = (1<<k)-1;
+
+        int ans =1;
+        if(mx!=a){
+            cout << mx << '\n';
+
+            /*
+                suppose we asked to calculate b in this case;
+
+                b = mx -a; just opposite of a set bits
+            
+            */
         }
         else{
-            //
+
+            for(int i=2;i*i<=a;i++){
+                if(a%i==0){
+                    ans = a/i;
+                    break;
+                }
+            }
+
+            cout << ans << "\n";
         }
+
+        
+
+
+
+
+
+
     }
 
     return 0;
