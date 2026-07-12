495f7aadd82e39b773a133feefdb674c647c3562
rishabh sinha <rishabh.sinha0709@gmail.com>
2026-07-12 16:12:19 +0530

    number theory practice upsolve

--- G_Floor_and_Mod.cpp
+++ G_Floor_and_Mod.cpp
@@ -4,22 +4,27 @@ using namespace std;
 typedef long long ll;
 
 
-ll solve(ll x, ll y){
+// ll solve(ll x, ll y){
 
-    ll cnt =0;
+//     ll cnt =0;
 
-    for(ll i=1;i<=x;i++){
-        for(ll j=1;j<=y;j++){
+//     for(ll i=1;i<=x;i++){
+//         for(ll j=1;j<=y;j++){
             
-            if(floor(i/j)==(i%j)) cnt++;
-        }
-    }
+//             if(floor(i/j)==(i%j)) cnt++;
+//         }
+//     }
 
-    return cnt;
+//     return cnt;
 
-    // cnt the pairs of co-primes such that a<=x and b<=y
-}
+//     // cnt the pairs of co-primes such that a<=x and b<=y
+// }
 
+
+/*
+    I thought for co-prime logic but look at the cases where k =2 or 3... or so on
+    it will be valid for k=1, and there we odn't need spf, every altenate number is co-prime
+*/
 int main() {
 
     ios::sync_with_stdio(false);
@@ -30,7 +35,13 @@ int main() {
     while(t--){
         ll x,y; cin>>x>>y;
 
-        cout << solve(x,y) << "\n";
+        ll ans =0;
+
+        for(int i=1;i*i<x;i++){
+            ans += max(0ll, min(y,x/i -1)-i);
+        }
+
+        cout << ans << '\n';
     }
 
     return 0;
