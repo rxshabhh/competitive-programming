3136e7a62590117895acd31db99b03b69b877b43
rishabh sinha <rishabh.sinha0709@gmail.com>
2026-09-11 02:09:23 +0530

    updated tree template

--- templates/treesSkeleton.cpp
+++ templates/treesSkeleton.cpp
@@ -131,6 +131,30 @@ void bfs(int root){
 
 }
 
+// Printing kth parent of any node for q queries
+void dp_parent(int node, int par, vector<vector<int>>& adj, vector<vector<int>>& dp){
+
+    dp[node] =0;
+
+    for(int i=1;i<=16;i++){ // log n pre-computation
+
+        dp[node][i] = dp[dp[node][i-1]][i-1];
+    }
+
+    for(auto child : adj[node]){
+
+        if(child == par){
+            continue;
+        }
+
+        dfs(child,node,adj,dp);
+    }
+
+    // for each node we have 17 parents 
+
+
+}
+
 
 
 void solve(){
@@ -146,10 +170,110 @@ void solve(){
 
 }
 
+// Printing kth parent for q queries
+
+int getKpar(int node, int k){
+
+
+    
+    for(int i=16;i>=0;i--){
+        if((k>>i) & 1){  // if ith bit is set in k
+            node = dp[node][i];
+        }
+    }
+
+    return node;
+
+}
+
+// LCA precomputation of level + solve3 function
+
+void dp_parent(int node, int par, vector<vector<int>>& adj, vector<vector<int>>& dp,vector<int>& level, int l=0){
+
+    dp[node] =0;
+    
+    // dp table calculation
+    for(int i=1;i<=16;i++){ // log n pre-computation
+
+        dp[node][i] = dp[dp[node][i-1]][i-1];
+    }
+
+    level[node] = l;
+
+    for(auto child : adj[node]){
+
+        if(child == par){
+            continue;
+        }
+
+        dfs(child,node,adj,dp,level,l+1);
+
+        l
+    }
+
+    // for each node we have 17 parents 
+
+
+}
+
+void solve3(){
+
+    // define adj, and all the basic things
+
+    vector<vector<int>> dp(n+1,vector<int>(17,0));  // for printing kth parent of any node for k queries
+
+    vector<int> level(n+1,0);
+    dp_parent(1,0,adj,dp,level);
+
+    // suppose queries is q
+    while(q--){
+
+        int a,b; cin>>a>>b;
+
+        if(level[a]>level[b]) swap(a,b);
+
+        // a is at lesser level
+        
+        int k= level[b]-level[a];
+
+        b = getKpar(b,k);
+
+        // a and b are at same level
+
+        if(a==b){
+            cout << a << endl;
+            continue;
+        }
+
+        for(int i=16;i>=1;i--){
+
+            if(dp[a][i] != dp[b][i]){
+                a = dp[a][i];
+                b = dp[b][i];
+            }
+
+
+        }
+
+        cout << dp[a][0] << endl;
+
+
+        
+    }
+}
+
+
+
 int main() {
 
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+
+    
+
+    
+
+
 
     
 
